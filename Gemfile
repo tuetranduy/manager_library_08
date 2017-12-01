d576caa6e09090d7f1950de688d6d41edcbeb1ae
tuetranduy <tue1996@gmail.com>
2017-12-01 14:02:31 +0700

    Add home, author, publisher pages

--- Gemfile
+++ Gemfile
@@ -5,14 +5,19 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+gem "bootstrap-sass", "3.3.7"
+gem "bootstrap-will_paginate", "1.0.0"
 gem "coffee-rails", "~> 4.2"
 gem "jbuilder", "~> 2.5"
+gem "rubocop", require: false
 gem "mysql2", ">= 0.3.18", "< 0.5"
 gem "puma", "~> 3.7"
 gem "rails", "~> 5.1.4"
 gem "sass-rails", "~> 5.0"
 gem "turbolinks", "~> 5"
 gem "uglifier", ">= 1.3.0"
+gem "figaro"
+gem "will_paginate", "3.1.5"
 group :development, :test do
   gem "byebug", platforms: [:mri, :mingw, :x64_mingw]
   gem "capybara", "~> 2.13"
@@ -24,6 +29,10 @@ group :development do
   gem "spring"
   gem "spring-watcher-listen", "~> 2.0.0"
   gem "web-console", ">= 3.3.0"
+end
+
+group :production do
+  gem "pg", "0.18.4"
 end
 
 gem "tzinfo-data", platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,16 @@ class User < ApplicationRecord
   has_many :books, through: :borrows
   has_many :likes, dependent: :destroy
   has_many :rating, dependent: :destroy
-  has_many :user_active_relationships, class_name: UserFollowRelationships.name,
+  has_many :user_active_relationships, class_name: UserFollowRelationShip.name,
    foreign_key: :follower_id, dependent: :destroy
-  has_many :user_passive_relationships, class_name: UserFollowRelationships.name,
+  has_many :user_passive_relationships, class_name: UserFollowRelationShip.name,
    foreign_key: :followed_id, dependent: :destroy
   has_many :followers, through: :user_active_relationships
   has_many :following, through: :user_passive_relationships, source: :followed
-  has_many :book_active_relationships, class_name: BookFollowerRelationships.name,
+  has_many :book_active_relationships, class_name: BookFollowerRelationship.name,
    foreign_key: :follower_id, dependent: :destroy
   has_many :follower_books, through: :book_active_relationships
-  has_many :author_active_relationships, class_name: AuthorFollowRelationships.name,
+  has_many :author_active_relationships, class_name: AuthorFollowRelationship.name,
    foreign_key: :follower_id, dependent: :destroy
   has_many :follower_authors, through: :author_active_relationships
   has_many :comments, dependent: :destroy
